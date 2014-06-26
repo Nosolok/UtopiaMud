55141091ecabbd1cf5f2726cf6258ae24d9a7879
Petr Karmashev (Rottenwood) <petr@quank.org>
2014-06-27 03:51:54 +0400

    Система поиска команд по первым символам

--- src/Rottenwood/UtopiaMudBundle/Service/CommandService.php
+++ src/Rottenwood/UtopiaMudBundle/Service/CommandService.php
@@ -33,7 +33,7 @@ class CommandService {
         $commands = Yaml::parse(file_get_contents($path));
 
         // разбитие команды на символы и их подсчет
-        $count =  count(preg_split('/(?<!^)(?!$)/u', $command ));
+        $count = count(preg_split('/(?<!^)(?!$)/u', $command));
 
         // проверка существования команды
         if ($run = $this->recursive_array_search($command, $commands["commands"])) {
@@ -44,6 +44,7 @@ class CommandService {
             $result["message"] = "0:1"; // команда не найдена
         }
 
+        $result["test"] = $this->recursive_array_search_substr($command, $commands["commands"], $count);
         $result["count"] = $count;
 
         return $result;
@@ -55,10 +56,32 @@ class CommandService {
      * @param $haystack
      * @return bool|int|string
      */
-    public function recursive_array_search($needle,$haystack) {
-        foreach($haystack as $key=>$value) {
-            $current_key=$key;
-            if($needle===$value OR (is_array($value) && $this->recursive_array_search($needle,$value) !== false)) {
+    public function recursive_array_search($needle, $haystack) {
+        foreach ($haystack as $key => $value) {
+            $current_key = $key;
+            if ($needle === $value OR (is_array($value) && $this->recursive_array_search($needle, $value) !== false)) {
+                return $current_key;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Рекурсивный поиск в массиве, используя первые X символов
+     * Входные параметры: иголка, стог сена, количество символо
+     * @param $needle
+     * @param $haystack
+     * @param $substr
+     * @return bool|int|string
+     */
+    public function recursive_array_search_substr($needle, $haystack, $substr) {
+        foreach ($haystack as $key => $value) {
+            if (is_string($value)) {
+                $value = mb_substr($value, 0, $substr,"utf-8");
+            }
+            $current_key = $key;
+            if ($needle === $value OR (is_array($value) && $this->recursive_array_search_substr
+                        ($needle, $value, $substr) !== false)) {
                 return $current_key;
             }
         }
