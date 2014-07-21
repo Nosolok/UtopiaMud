d80492b94e6c73831998c245fbf8221bc8a34e2b
Petr Karmashev (Rottenwood) <petr@quank.org>
2014-07-22 01:16:28 +0400

    Создание npc-крысы #26

--- src/Rottenwood/UtopiaMudBundle/Entity/Mob.php
+++ src/Rottenwood/UtopiaMudBundle/Entity/Mob.php
@@ -6,7 +6,7 @@ use Doctrine\ORM\Mapping as ORM;
 
 /**
  * Mob
- * @ORM\Table()
+ * @ORM\Table(name="mobs")
  * @ORM\Entity(repositoryClass="Rottenwood\UtopiaMudBundle\Entity\MobRepository")
  */
 class Mob {
@@ -26,12 +26,6 @@ class Mob {
     private $name;
 
     /**
-     * @var string
-     * @ORM\Column(name="longdesc", type="string", length=500)
-     */
-    private $longdesc;
-
-    /**
      * @ORM\ManyToOne(targetEntity="Race")
      */
     private $race;
@@ -43,35 +37,22 @@ class Mob {
     private $sex;
 
     /**
-     * @var integer
-     * @ORM\Column(name="ST", type="integer")
-     */
-    private $sT;
-
-    /**
-     * @var integer
-     * @ORM\Column(name="IQ", type="integer")
-     */
-    private $iQ;
-
-    /**
-     * @var integer
-     * @ORM\Column(name="DX", type="integer")
-     */
-    private $dX;
-
-    /**
-     * @var integer
-     * @ORM\Column(name="HT", type="integer")
-     */
-    private $hT;
-
-    /**
      * @var string
      * @ORM\Column(name="shortdesc", type="string", length=255)
      */
     private $shortdesc;
 
+    /**
+     * @var string
+     * @ORM\Column(name="longdesc", type="string", length=500)
+     */
+    private $longdesc;
+
+    /**
+     * @var string
+     * @ORM\Column(name="zone", type="string", length=255)
+     */
+    private $zone;
 
     /**
      * Get id
@@ -98,25 +79,6 @@ class Mob {
      */
     public function getName() {
         return $this->name;
-    }
-
-    /**
-     * Set longdesc
-     * @param string $longdesc
-     * @return Mob
-     */
-    public function setLongdesc($longdesc) {
-        $this->longdesc = $longdesc;
-
-        return $this;
-    }
-
-    /**
-     * Get longdesc
-     * @return string
-     */
-    public function getLongdesc() {
-        return $this->longdesc;
     }
 
     /**
@@ -158,82 +120,6 @@ class Mob {
     }
 
     /**
-     * Set sT
-     * @param integer $sT
-     * @return Mob
-     */
-    public function setST($sT) {
-        $this->sT = $sT;
-
-        return $this;
-    }
-
-    /**
-     * Get sT
-     * @return integer
-     */
-    public function getST() {
-        return $this->sT;
-    }
-
-    /**
-     * Set iQ
-     * @param integer $iQ
-     * @return Mob
-     */
-    public function setIQ($iQ) {
-        $this->iQ = $iQ;
-
-        return $this;
-    }
-
-    /**
-     * Get iQ
-     * @return integer
-     */
-    public function getIQ() {
-        return $this->iQ;
-    }
-
-    /**
-     * Set dX
-     * @param integer $dX
-     * @return Mob
-     */
-    public function setDX($dX) {
-        $this->dX = $dX;
-
-        return $this;
-    }
-
-    /**
-     * Get dX
-     * @return integer
-     */
-    public function getDX() {
-        return $this->dX;
-    }
-
-    /**
-     * Set hT
-     * @param integer $hT
-     * @return Mob
-     */
-    public function setHT($hT) {
-        $this->hT = $hT;
-
-        return $this;
-    }
-
-    /**
-     * Get hT
-     * @return integer
-     */
-    public function getHT() {
-        return $this->hT;
-    }
-
-    /**
      * Set shortdesc
      * @param string $shortdesc
      * @return Mob
@@ -250,5 +136,38 @@ class Mob {
      */
     public function getShortdesc() {
         return $this->shortdesc;
+    }
+
+    /**
+     * Set longdesc
+     * @param string $longdesc
+     * @return Mob
+     */
+    public function setLongdesc($longdesc) {
+        $this->longdesc = $longdesc;
+
+        return $this;
+    }
+
+    /**
+     * Get longdesc
+     * @return string
+     */
+    public function getLongdesc() {
+        return $this->longdesc;
+    }
+
+    /**
+     * @return string
+     */
+    public function getZone() {
+        return $this->zone;
+    }
+
+    /**
+     * @param string $zone
+     */
+    public function setZone($zone) {
+        $this->zone = $zone;
     }
 }
